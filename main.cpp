8c9caf3ac9cf5ab9e7dcadd53dd3f6512b2b336b
AliaksandraPashko <aliaksandra.pashko@solarwinds.com>
2018-09-29 08:39:00 +0300

    change template print generated values to base class virtual function call

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include "Poisson.h"
 #include "Binomial.h"
 #include "NegativeBinomial.h"
+#include "Geometric.h"
+#include "Bernoulli.h"
 
-template <class Distribution>
-void print_generated_values(Distribution distribution)
+void print_generated_values(Distribution* distribution)
 {
-    std::vector<int> result = distribution.generate();
+    std::vector<int> result = distribution->generate();
     std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
 
     std::cout << std::endl;
@@ -14,14 +15,20 @@ void print_generated_values(Distribution distribution)
 
 int main() {
 
-    Poisson poisson_distribution(1);
+    Distribution* poisson_distribution = new Poisson(1);
 //    print_generated_values(poisson_distribution);
 
-    Binomial binomial_distribution(5, 0.75);
+    Distribution* binomial_distribution = new Binomial(5, 0.75);
 //    print_generated_values(binomial_distribution);
 
-    NegativeBinomial negativeBinomial_distribution(5, 0.9);
+    Distribution* negativeBinomial_distribution = new NegativeBinomial(5, 0.9);
 //    print_generated_values(negativeBinomial_distribution);
+
+    Distribution* geometric_distribution = new Geometric(0.7);
+//    print_generated_values(geometric_distribution);
+
+    Distribution* bernoulli_distribution = new Bernoulli(0.6);
+//    print_generated_values(bernoulli_distribution);
 
     return 0;
 }
